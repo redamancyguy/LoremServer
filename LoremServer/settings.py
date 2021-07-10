dcfc947ef158ae206eb916d71fad3e70ccf196f5
1506607292@qq.com <1506607292@qq.com>
2021-07-10 09:22:47 +0800

    s

--- LoremServer/settings.py
+++ LoremServer/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'middlewares.csrf.CsrfVerifyMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
